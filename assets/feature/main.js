6967ae3221632b9fc1ea89e1827431a899a92d73
ionsqr <iono@disroot.org>
2022-07-01 20:02:04 +0700

    clean

--- assets/feature/main.js
+++ assets/feature/main.js
@@ -1,12 +1,10 @@
 import lightGallery from 'feature/lightgallery/lightgallery.umd.js'
-import lgThumbnail from 'feature/lightgallery/plugins/lg-thumbnail.min.js'
-import lgZoom from 'feature/lightgallery/plugins/lg-zoom.min.js'
 import 'feature/instant.page.js'
 import clipboard from 'feature/clipboard.js'
+
 
 lightGallery(document.getElementById('lightgallery'), {
 
         selector: '.photo-item',
-        mode: 'lg-zoom-in-big',
         slideEndAnimatoin: 'false',
     });
