7fe0683259322066ae6ccc4bb96971ea2b1cbcee
ionsqr <iono@disroot.org>
2022-08-16 21:39:48 +0700

    edit style

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+import lightGallery from 'js/lightgallery/lightgallery.umd.js'
+import 'js/instant.page.js'
+import 'js/code-name.js'
+import 'js/darkmode.js'
+import 'js/clipboard.js'
+
+
+lightGallery(document.getElementById('lightgallery'), {
+
+        selector: '.photo-item',
+        slideEndAnimatoin: 'false',
+    });
