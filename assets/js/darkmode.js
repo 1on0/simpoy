d6d29bee8863a1c3f05d2d562bf2c88ce93c3beb
ionsqr <iono@disroot.org>
2022-12-05 11:10:32 +0700

    remove unused style

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -1,1 +1,20 @@
-function setTheme(e){sessionStorage.setItem("dark-mode-storage",e),"dark"==e?document.body.classList.add("dark"):"light"==e&&document.body.classList.remove("dark")}var savedTheme=sessionStorage.getItem("dark-mode-storage")||"light",toggle=(setTheme(savedTheme),document.getElementById("darkmode"));toggle.addEventListener("click",()=>{document.body.classList.contains("dark")?setTheme("light"):setTheme("dark")});
+const btn = document.querySelector(".darkmode");
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+const currentTheme = sessionStorage.getItem("theme");
+if (currentTheme == "dark") {
+  document.body.classList.toggle("dark");
+} else if (currentTheme == "light") {
+  document.body.classList.toggle("light");
+}
+
+btn.addEventListener("click", function() {
+  if (prefersDarkScheme.matches) {
+    document.body.classList.toggle("light");
+    var theme = document.body.classList.contains("light") ? "light" : "dark";
+  } else {
+    document.body.classList.toggle("dark");
+    var theme = document.body.classList.contains("dark") ? "dark" : "light";
+  }
+  sessionStorage.setItem("theme", theme);
+});
